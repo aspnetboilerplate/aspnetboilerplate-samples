92d243a0d4c3c5dd2e73593dbab68d2662bf6aef
hikalkan <hi_kalkan@yahoo.com>
2014-06-22 21:25:22 +0300

    Fixes

--- SimpleTaskSystemSPA_AngularJs_EntityFramework/SimpleTaskSystem.Infrastructure.EntityFramework/EntityFramework/Repositories/PersonRepository.cs
+++ SimpleTaskSystemSPA_AngularJs_EntityFramework/SimpleTaskSystem.Infrastructure.EntityFramework/EntityFramework/Repositories/PersonRepository.cs
@@ -1,22 +1,9 @@
-﻿using Abp.Domain.Entities;
-using Abp.Domain.Repositories.EntityFramework;
-using SimpleTaskSystem.People;
+﻿using SimpleTaskSystem.People;
 
 namespace SimpleTaskSystem.EntityFramework.Repositories
 {
     public class PersonRepository : SimpleTaskSystemRepositoryBase<Person>, IPersonRepository
     {
 
-    }
-
-    public class SimpleTaskSystemRepositoryBase<TEntity> : SimpleTaskSystemRepositoryBase<TEntity, int>
-        where TEntity : class, IEntity<int>
-    {
-        
-    }
-
-    public class SimpleTaskSystemRepositoryBase<TEntity, TPrimaryKey> : EfRepositoryBase<SimpleTaskSystemDbContext, TEntity, TPrimaryKey>
-        where TEntity : class, IEntity<TPrimaryKey>
-    {
     }
 }
---
+++ SimpleTaskSystemSPA_AngularJs_EntityFramework/SimpleTaskSystem.Infrastructure.EntityFramework/EntityFramework/Repositories/SimpleTaskSystemRepositoryBase.cs
@@ -0,0 +1,16 @@
+﻿using Abp.Domain.Entities;
+using Abp.Domain.Repositories.EntityFramework;
+
+namespace SimpleTaskSystem.EntityFramework.Repositories
+{
+    public abstract class SimpleTaskSystemRepositoryBase<TEntity, TPrimaryKey> : EfRepositoryBase<SimpleTaskSystemDbContext, TEntity, TPrimaryKey>
+        where TEntity : class, IEntity<TPrimaryKey>
+    {
+    }
+
+    public abstract class SimpleTaskSystemRepositoryBase<TEntity> : SimpleTaskSystemRepositoryBase<TEntity, int>
+        where TEntity : class, IEntity<int>
+    {
+        
+    }
+}
