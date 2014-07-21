631c0e697f1427cef9d6d95308866db516e61b1c
hikalkan <hi_kalkan@yahoo.com>
2014-07-21 16:10:22 +0300

    Updated to ABP v0.3.

--- SimpleTaskSystemSPA_Durandal_NHibernate/SimpleTaskSystem.Infrastructure.NHibernate/NHibernate/EntityMappings/TaskMap.cs
+++ SimpleTaskSystemSPA_Durandal_NHibernate/SimpleTaskSystem.Infrastructure.NHibernate/NHibernate/EntityMappings/TaskMap.cs
@@ -3,15 +3,15 @@ using SimpleTaskSystem.Tasks;
 
 namespace SimpleTaskSystem.NHibernate.EntityMappings
 {
-public class TaskMap : EntityMap<Task, long>
-{
-    public TaskMap()
-        : base("StsTasks")
+    public class TaskMap : EntityMap<Task, long>
     {
-        Map(x => x.Description);
-        Map(x => x.CreationTime);
-        Map(x => x.State).CustomType<TaskState>();
-        References(x => x.AssignedPerson).Column("AssignedPersonId").LazyLoad();
+        public TaskMap()
+            : base("StsTasks")
+        {
+            Map(x => x.Description);
+            Map(x => x.CreationTime);
+            Map(x => x.State).CustomType<TaskState>();
+            References(x => x.AssignedPerson).Column("AssignedPersonId").LazyLoad();
+        }
     }
-}
 }
