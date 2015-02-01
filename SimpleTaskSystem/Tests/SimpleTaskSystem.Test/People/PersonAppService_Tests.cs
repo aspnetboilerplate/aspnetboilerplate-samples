a9d2d0fb27485be4a6a1e31add1489e1bfb84b9d
hikalkan <hi_kalkan@yahoo.com>
2015-02-01 14:45:33 +0200

    Added test for PersonAppService.

---
+++ SimpleTaskSystem/Tests/SimpleTaskSystem.Test/People/PersonAppService_Tests.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Shouldly;
+using SimpleTaskSystem.People;
+using Xunit;
+
+namespace SimpleTaskSystem.Test.People
+{
+    public class PersonAppService_Tests : SimpleTaskSystemTestBase
+    {
+        private readonly IPersonAppService _personAppService;
+
+        public PersonAppService_Tests()
+        {
+            _personAppService = LocalIocManager.Resolve<IPersonAppService>();
+        }
+
+        [Fact]
+        public async Task Should_Get_All_People()
+        {
+            var output = await _personAppService.GetAllPeople();
+            output.People.Count.ShouldBe(4);
+        }
+    }
+}
