1af6af04c766c939a1391287fa3195134ce80745
hikalkan <hi_kalkan@yahoo.com>
2015-06-09 19:28:42 +0300

    Added console application for SimpleTaskSystem.

--- SimpleTaskSystem/SimpleTaskSystem.Application/Tasks/Dtos/TaskDto.cs
+++ SimpleTaskSystem/SimpleTaskSystem.Application/Tasks/Dtos/TaskDto.cs
@@ -1,5 +1,6 @@
 using System;
 using Abp.Application.Services.Dto;
+using Abp.Extensions;
 
 namespace SimpleTaskSystem.Tasks.Dtos
 {
@@ -17,5 +18,18 @@ namespace SimpleTaskSystem.Tasks.Dtos
         public DateTime CreationTime { get; set; }
 
         public byte State { get; set; }
+
+        //This method is just used by the Console Application to list tasks
+        public override string ToString()
+        {
+            return string.Format(
+                "[Task Id={0}, Description={1}, CreationTime={2}, AssignedPersonName={3}, State={4}]",
+                Id,
+                Description,
+                CreationTime,
+                AssignedPersonId,
+                (TaskState)State
+                );
+        }
     }
 }
---
+++ SimpleTaskSystem/SimpleTaskSystem.ConsoleApp/SimpleTaskSystemConsoleAppModule.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using Abp.Modules;
+
+namespace SimpleTaskSystem.ConsoleApp
+{
+    [DependsOn(typeof(SimpleTaskSystemDataModule), typeof(SimpleTaskSystemApplicationModule))]
+    public class SimpleTaskSystemConsoleAppModule : AbpModule
+    {
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
+        }
+    }
+}
