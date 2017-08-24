e75ad97572e499641fcfd5aa7d1e4d9e00ea020a
yekalkan <emre.kalkan@volosoft.com>
2017-08-24 14:26:26 +0300

    TestResultAdded
    TestResultAdded

---
+++ TestProjects/TesterApp/TesterApp/TestResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TesterApp
+{
+    public class TestResult
+    {
+        public bool Success { get; set; }
+
+        public string Method { get; set; }
+
+        public TimeSpan Duration { get; set; }
+
+        public string ErrorMessage { get; set; }
+    }
+}
