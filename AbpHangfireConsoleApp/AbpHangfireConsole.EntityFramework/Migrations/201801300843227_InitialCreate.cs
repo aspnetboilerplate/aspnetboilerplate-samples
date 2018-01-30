1168efc9810f2c85dd7d319f3f9a30d4842b9734
Donovan Edye <donovan@natiki.com.au>
2018-01-30 19:46:13 +1100

    Added initial commit so migrations will work.

---
+++ AbpHangfireConsoleApp/AbpHangfireConsole.EntityFramework/Migrations/201801300843227_InitialCreate.cs
@@ -0,0 +1,16 @@
+namespace AbpHangfireConsole.EntityFramework.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
