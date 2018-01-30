afa00d8bba132ef26ad6322813f85509370dd202
İsmail ÇAGDAŞ <ismailcagdas@gmail.com>
2018-01-30 16:06:00 +0300

    finished displaying heroes on the angular ui

--- AngularUniversal/aspnet-core/src/Acme.HeroShop.Web.Public/HeroApp/src/app/app.component.ts
+++ AngularUniversal/aspnet-core/src/Acme.HeroShop.Web.Public/HeroApp/src/app/app.component.ts
@@ -8,15 +8,26 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
 
   heroCompanies: any = [];
-  heroes: any = [];
+  originalHeroes: any = [];
+  displayHeroes: any = [];
+
+  selectedHeroCompany: string = 'All';
+
 
   ngOnInit(): void {
     this.heroCompanies = (window as any).TRANSFER_CACHE.fromDotnet.heroCompanies;
-    this.heroes = (window as any).TRANSFER_CACHE.fromDotnet.heroes;
+    this.originalHeroes = (window as any).TRANSFER_CACHE.fromDotnet.heroes;
+    this.displayHeroes = (window as any).TRANSFER_CACHE.fromDotnet.heroes;
   }
-  
+
   filterHeroes(heroCompanyId: number): void {
-    //todo: ger heroes from server here...
+    if (!heroCompanyId) {
+      this.displayHeroes = this.originalHeroes;
+      this.selectedHeroCompany = 'All';
+    } else {
+      this.displayHeroes = this.originalHeroes.filter(hero => hero.heroCompanyId === heroCompanyId);
+      this.selectedHeroCompany = this.heroCompanies.filter(heroCompany => heroCompany.id === heroCompanyId)[0].name;
+    }
   }
 
 }
