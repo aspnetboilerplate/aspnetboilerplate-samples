be8caa79be803c4787b6072e3a873ebdee1a0fbb
yekalkan <emre.kalkan@volosoft.com>
2018-01-31 14:57:47 +0300

    InterceptionDemo Upgraded To Abp 3.4.0

---
+++ InterceptionDemo/InterceptionDemo.EntityFramework/Migrations/201801311156239_Upgraded_To_Abp_3.4.0.cs
@@ -0,0 +1,132 @@
+namespace InterceptionDemo.Migrations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity.Infrastructure.Annotations;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Upgraded_To_Abp_340 : DbMigration
+    {
+        public override void Up()
+        {
+            AlterTableAnnotations(
+                "dbo.AbpFeatures",
+                c => new
+                    {
+                        Id = c.Long(nullable: false, identity: true),
+                        TenantId = c.Int(),
+                        Name = c.String(nullable: false, maxLength: 128),
+                        Value = c.String(nullable: false, maxLength: 2000),
+                        CreationTime = c.DateTime(nullable: false),
+                        CreatorUserId = c.Long(),
+                        EditionId = c.Int(),
+                        Discriminator = c.String(nullable: false, maxLength: 128),
+                    },
+                annotations: new Dictionary<string, AnnotationValues>
+                {
+                    { 
+                        "DynamicFilter_EditionFeatureSetting_MayHaveTenant",
+                        new AnnotationValues(oldValue: null, newValue: "EntityFramework.DynamicFilters.DynamicFilterDefinition")
+                    },
+                    { 
+                        "DynamicFilter_FeatureSetting_MayHaveTenant",
+                        new AnnotationValues(oldValue: null, newValue: "EntityFramework.DynamicFilters.DynamicFilterDefinition")
+                    },
+                    { 
+                        "DynamicFilter_TenantFeatureSetting_MayHaveTenant",
+                        new AnnotationValues(oldValue: null, newValue: "EntityFramework.DynamicFilters.DynamicFilterDefinition")
+                    },
+                    { 
+                        "DynamicFilter_TenantFeatureSetting_MustHaveTenant",
+                        new AnnotationValues(oldValue: "EntityFramework.DynamicFilters.DynamicFilterDefinition", newValue: null)
+                    },
+                });
+            
+            AlterTableAnnotations(
+                "dbo.AbpUserOrganizationUnits",
+                c => new
+                    {
+                        Id = c.Long(nullable: false, identity: true),
+                        TenantId = c.Int(),
+                        UserId = c.Long(nullable: false),
+                        OrganizationUnitId = c.Long(nullable: false),
+                        IsDeleted = c.Boolean(nullable: false),
+                        CreationTime = c.DateTime(nullable: false),
+                        CreatorUserId = c.Long(),
+                    },
+                annotations: new Dictionary<string, AnnotationValues>
+                {
+                    { 
+                        "DynamicFilter_UserOrganizationUnit_SoftDelete",
+                        new AnnotationValues(oldValue: null, newValue: "EntityFramework.DynamicFilters.DynamicFilterDefinition")
+                    },
+                });
+            
+            AddColumn("dbo.AbpUserOrganizationUnits", "IsDeleted", c => c.Boolean(nullable: false));
+            AlterColumn("dbo.AbpUserClaims", "ClaimType", c => c.String(maxLength: 256));
+            AlterColumn("dbo.AbpUserAccounts", "UserName", c => c.String(maxLength: 32));
+            AlterColumn("dbo.AbpUserAccounts", "EmailAddress", c => c.String(maxLength: 256));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.AbpUserAccounts", "EmailAddress", c => c.String());
+            AlterColumn("dbo.AbpUserAccounts", "UserName", c => c.String());
+            AlterColumn("dbo.AbpUserClaims", "ClaimType", c => c.String());
+            DropColumn("dbo.AbpUserOrganizationUnits", "IsDeleted");
+            AlterTableAnnotations(
+                "dbo.AbpUserOrganizationUnits",
+                c => new
+                    {
+                        Id = c.Long(nullable: false, identity: true),
+                        TenantId = c.Int(),
+                        UserId = c.Long(nullable: false),
+                        OrganizationUnitId = c.Long(nullable: false),
+                        IsDeleted = c.Boolean(nullable: false),
+                        CreationTime = c.DateTime(nullable: false),
+                        CreatorUserId = c.Long(),
+                    },
+                annotations: new Dictionary<string, AnnotationValues>
+                {
+                    { 
+                        "DynamicFilter_UserOrganizationUnit_SoftDelete",
+                        new AnnotationValues(oldValue: "EntityFramework.DynamicFilters.DynamicFilterDefinition", newValue: null)
+                    },
+                });
+            
+            AlterTableAnnotations(
+                "dbo.AbpFeatures",
+                c => new
+                    {
+                        Id = c.Long(nullable: false, identity: true),
+                        TenantId = c.Int(),
+                        Name = c.String(nullable: false, maxLength: 128),
+                        Value = c.String(nullable: false, maxLength: 2000),
+                        CreationTime = c.DateTime(nullable: false),
+                        CreatorUserId = c.Long(),
+                        EditionId = c.Int(),
+                        Discriminator = c.String(nullable: false, maxLength: 128),
+                    },
+                annotations: new Dictionary<string, AnnotationValues>
+                {
+                    { 
+                        "DynamicFilter_EditionFeatureSetting_MayHaveTenant",
+                        new AnnotationValues(oldValue: "EntityFramework.DynamicFilters.DynamicFilterDefinition", newValue: null)
+                    },
+                    { 
+                        "DynamicFilter_FeatureSetting_MayHaveTenant",
+                        new AnnotationValues(oldValue: "EntityFramework.DynamicFilters.DynamicFilterDefinition", newValue: null)
+                    },
+                    { 
+                        "DynamicFilter_TenantFeatureSetting_MayHaveTenant",
+                        new AnnotationValues(oldValue: "EntityFramework.DynamicFilters.DynamicFilterDefinition", newValue: null)
+                    },
+                    { 
+                        "DynamicFilter_TenantFeatureSetting_MustHaveTenant",
+                        new AnnotationValues(oldValue: null, newValue: "EntityFramework.DynamicFilters.DynamicFilterDefinition")
+                    },
+                });
+            
+        }
+    }
+}
