653ace73eb8bcb6eca71261868efa8a2fdef3e33
İsmail ÇAGDAŞ <ismailcagdas@gmail.com>
2018-02-02 16:22:54 +0300

    Updated OrganizationUnitsDemo demo to ABP v3.4

--- OrganizationUnitsDemo/OrganizationUnitsDemo.Core/Users/UserManager.cs
+++ OrganizationUnitsDemo/OrganizationUnitsDemo.Core/Users/UserManager.cs
@@ -1,51 +1,44 @@
 ﻿using Abp.Authorization;
 using Abp.Authorization.Users;
 using Abp.Configuration;
-using Abp.Configuration.Startup;
-using Abp.Dependency;
 using Abp.Domain.Repositories;
 using Abp.Domain.Uow;
+using Abp.IdentityFramework;
+using Abp.Localization;
 using Abp.Organizations;
 using Abp.Runtime.Caching;
-using Abp.Zero.Configuration;
 using OrganizationUnitsDemo.Authorization.Roles;
-using OrganizationUnitsDemo.MultiTenancy;
 
 namespace OrganizationUnitsDemo.Users
 {
-    public class UserManager : AbpUserManager<Tenant, Role, User>
+    public class UserManager : AbpUserManager<Role, User>
     {
         public UserManager(
-            UserStore store,
+            UserStore userStore,
             RoleManager roleManager,
-            IRepository<Tenant> tenantRepository,
-            IMultiTenancyConfig multiTenancyConfig,
             IPermissionManager permissionManager,
             IUnitOfWorkManager unitOfWorkManager,
-            ISettingManager settingManager,
-            IUserManagementConfig userManagementConfig,
-            IIocResolver iocResolver,
             ICacheManager cacheManager,
             IRepository<OrganizationUnit, long> organizationUnitRepository,
             IRepository<UserOrganizationUnit, long> userOrganizationUnitRepository,
             IOrganizationUnitSettings organizationUnitSettings,
-            IRepository<UserLoginAttempt, long> userLoginAttemptRepository)
+            IdentityEmailMessageService emailService,
+            ILocalizationManager localizationManager,
+            ISettingManager settingManager,
+            IUserTokenProviderAccessor userTokenProviderAccessor)
             : base(
-                store,
+                userStore,
                 roleManager,
-                tenantRepository,
-                multiTenancyConfig,
                 permissionManager,
                 unitOfWorkManager,
-                settingManager,
-                userManagementConfig,
-                iocResolver,
                 cacheManager,
                 organizationUnitRepository,
                 userOrganizationUnitRepository,
                 organizationUnitSettings,
-                userLoginAttemptRepository
-            )
+                localizationManager,
+                emailService,
+                settingManager,
+                userTokenProviderAccessor)
         {
         }
     }
