4fdfa46a84ec8a0a75be1aeb098a76e99b515734
maliming <malimings@gmail.com>
2019-06-25 14:16:38 +0800

    Change the way to get the connection string.

--- MultipleDbContextEfCoreDemo/src/MultipleDbContextEfCoreDemo.EntityFrameworkCore/EntityFrameworkCore/MyConnectionStringResolver.cs
+++ MultipleDbContextEfCoreDemo/src/MultipleDbContextEfCoreDemo.EntityFrameworkCore/EntityFrameworkCore/MyConnectionStringResolver.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Abp.Configuration.Startup;
 using Abp.Domain.Uow;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using MultipleDbContextEfCoreDemo.Configuration;
 using MultipleDbContextEfCoreDemo.Web;
@@ -9,17 +10,20 @@ namespace MultipleDbContextEfCoreDemo.EntityFrameworkCore
 {
     public class MyConnectionStringResolver : DefaultConnectionStringResolver
     {
-        public MyConnectionStringResolver(IAbpStartupConfiguration configuration) 
+        private readonly IConfigurationRoot _appConfiguration;
+
+        public MyConnectionStringResolver(IAbpStartupConfiguration configuration,IHostingEnvironment hostingEnvironment) 
             : base(configuration)
         {
+            _appConfiguration =
+                AppConfigurations.Get(hostingEnvironment.ContentRootPath, hostingEnvironment.EnvironmentName);
         }
 
         public override string GetNameOrConnectionString(ConnectionStringResolveArgs args)
         {
             if (args["DbContextConcreteType"] as Type == typeof(MultipleDbContextEfCoreDemoSecondDbContext))
             {
-                var configuration = AppConfigurations.Get(WebContentDirectoryFinder.CalculateContentRootFolder());
-                return configuration.GetConnectionString(MultipleDbContextEfCoreDemoConsts.SecondDbConnectionStringName);
+                return _appConfiguration.GetConnectionString(MultipleDbContextEfCoreDemoConsts.SecondDbConnectionStringName);
             }
 
             return base.GetNameOrConnectionString(args);
