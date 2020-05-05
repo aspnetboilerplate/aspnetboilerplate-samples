551de743d6f7f1b5bc3521d716914ba2306bb252
Musa Demir <musa.demir@volosoft.com>
2020-05-05 18:53:35 +0300

    fix published bundle reference error

--- SimpleTaskSystem/SimpleTaskSystem.WebSpaAngular/App_Start/BundleConfig.cs
+++ SimpleTaskSystem/SimpleTaskSystem.WebSpaAngular/App_Start/BundleConfig.cs
@@ -13,15 +13,13 @@ namespace SimpleTaskSystem.WebSpaAngular
             //~/Bundles/App/vendor/css
             bundles.Add(
                 new StyleBundle("~/Bundles/App/vendor/css")
-                    .Include(
-                        "~/Content/themes/base/all.css",
-                        "~/Content/bootstrap-cosmo.min.css",
-                        "~/Content/toastr.min.css",
-                        "~/Scripts/sweetalert/sweet-alert.css",
-                        "~/Content/flags/famfamfam-flags.css",
-                        "~/Content/font-awesome.min.css"
-                    )
-                );
+                    .Include("~/Content/themes/base/all.css", new CssRewriteUrlTransform())
+                    .Include("~/Content/bootstrap-cosmo.min.css", new CssRewriteUrlTransform())
+                    .Include("~/Content/toastr.min.css")
+                    .Include("~/Scripts/sweetalert/sweet-alert.css")
+                    .Include("~/Content/flags/famfamfam-flags.css", new CssRewriteUrlTransform())
+                    .Include("~/Content/font-awesome.min.css", new CssRewriteUrlTransform())
+            );
 
             //~/Bundles/App/vendor/js
             bundles.Add(
