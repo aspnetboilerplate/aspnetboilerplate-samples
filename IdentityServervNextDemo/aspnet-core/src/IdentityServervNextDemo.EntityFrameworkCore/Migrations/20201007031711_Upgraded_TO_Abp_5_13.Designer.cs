ba7885bd709d4ed25b71469ea1ff644538788d67
maliming <6908465+maliming@users.noreply.github.com>
2020-10-07 12:56:21 +0800

    Use Abp 5.13.0 packages.

--- IdentityServervNextDemo/aspnet-core/src/IdentityServervNextDemo.EntityFrameworkCore/Migrations/20201007031711_Upgraded_TO_Abp_5_13.Designer.cs
+++ IdentityServervNextDemo/aspnet-core/src/IdentityServervNextDemo.EntityFrameworkCore/Migrations/20201007031711_Upgraded_TO_Abp_5_13.Designer.cs
@@ -4,14 +4,16 @@ using IdentityServervNextDemo.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace IdentityServervNextDemo.Migrations
 {
     [DbContext(typeof(IdentityServervNextDemoDbContext))]
-    partial class IdentityServervNextDemoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201007031711_Upgraded_TO_Abp_5_13")]
+    partial class Upgraded_TO_Abp_5_13
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
