33f1f3e560642df6d8137e6c53738c63affcc201
Musa Demir <demirmusa96@gmail.com>
2021-02-25 15:56:35 +0300

    Update AbpCoreEf6SampleDbContext.cs

--- AbpCoreEf6Sample/aspnet-core/src/AbpCoreEf6Sample.EntityFrameworkCore/EntityFrameworkCore/AbpCoreEf6SampleDbContext.cs
+++ AbpCoreEf6Sample/aspnet-core/src/AbpCoreEf6Sample.EntityFrameworkCore/EntityFrameworkCore/AbpCoreEf6SampleDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System.Data.Entity;
+﻿using System.Data.Common;
+using System.Data.Entity;
+using Abp.DynamicEntityProperties;
 using Abp.Zero.EntityFramework;
 using AbpCoreEf6Sample.Authorization.Roles;
 using AbpCoreEf6Sample.Authorization.Users;
@@ -8,11 +10,46 @@ namespace AbpCoreEf6Sample.EntityFrameworkCore
 {
     public class AbpCoreEf6SampleDbContext : AbpZeroDbContext<Tenant, Role, User, AbpCoreEf6SampleDbContext>
     {
-        /* Define a DbSet for each entity of the application */
-        
-        public AbpCoreEf6SampleDbContext(DbContextOptions<AbpCoreEf6SampleDbContext> options)
-            : base(options)
+        /* NOTE: 
+          *   Setting "Default" to base class helps us when working migration commands on Package Manager Console.
+          *   But it may cause problems when working Migrate.exe of EF. If you will apply migrations on command line, do not
+          *   pass connection string name to base classes. ABP works either way.
+          */
+        public AbpCoreEf6SampleDbContext()
+            : base("Default")
         {
+
+        }
+
+        /* NOTE:
+         *   This constructor is used by ABP to pass connection string defined in AbpCoreEf6SampleDataModule.PreInitialize.
+         *   Notice that, actually you will not directly create an instance of AbpCoreEf6SampleDbContext since ABP automatically handles it.
+         */
+        public AbpCoreEf6SampleDbContext(string nameOrConnectionString)
+            : base(nameOrConnectionString)
+        {
+
+        }
+
+        //This constructor is used in tests
+        public AbpCoreEf6SampleDbContext(DbConnection existingConnection)
+         : base(existingConnection, false)
+        {
+
+        }
+
+        public AbpCoreEf6SampleDbContext(DbConnection existingConnection, bool contextOwnsConnection)
+         : base(existingConnection, contextOwnsConnection)
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<DynamicProperty>().Property(p => p.PropertyName).HasMaxLength(250);
+            modelBuilder.Entity<DynamicEntityProperty>().Property(p => p.EntityFullName).HasMaxLength(250);
         }
     }
 }
