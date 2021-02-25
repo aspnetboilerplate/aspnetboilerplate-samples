d7bf46072312dd0c6f270b11c29fd37134b0b134
Musa Demir <demirmusa96@gmail.com>
2021-02-25 15:59:50 +0300

    Update AbpCoreEf6SampleEntityFrameworkModule.cs

--- AbpCoreEf6Sample/aspnet-core/src/AbpCoreEf6Sample.EntityFrameworkCore/EntityFrameworkCore/AbpCoreEf6SampleEntityFrameworkModule.cs
+++ AbpCoreEf6Sample/aspnet-core/src/AbpCoreEf6Sample.EntityFrameworkCore/EntityFrameworkCore/AbpCoreEf6SampleEntityFrameworkModule.cs
@@ -1,50 +1,26 @@
-﻿using Abp.EntityFrameworkCore.Configuration;
-using Abp.Modules;
+﻿using Abp.Modules;
 using Abp.Reflection.Extensions;
 using Abp.Zero.EntityFramework;
-using AbpCoreEf6Sample.EntityFrameworkCore.Seed;
+using System.Data.Entity;
 
 namespace AbpCoreEf6Sample.EntityFrameworkCore
 {
     [DependsOn(
         typeof(AbpCoreEf6SampleCoreModule), 
-        typeof(AbpZeroCoreEntityFrameworkCoreModule))]
+        typeof(AbpZeroCoreEntityFrameworkModule))]
     public class AbpCoreEf6SampleEntityFrameworkModule : AbpModule
     {
-        /* Used it tests to skip dbcontext registration, in order to use in-memory database of EF Core */
-        public bool SkipDbContextRegistration { get; set; }
-
-        public bool SkipDbSeed { get; set; }
-
+ 
         public override void PreInitialize()
         {
-            if (!SkipDbContextRegistration)
-            {
-                Configuration.Modules.AbpEfCore().AddDbContext<AbpCoreEf6SampleDbContext>(options =>
-                {
-                    if (options.ExistingConnection != null)
-                    {
-                        AbpCoreEf6SampleDbContextConfigurer.Configure(options.DbContextOptions, options.ExistingConnection);
-                    }
-                    else
-                    {
-                        AbpCoreEf6SampleDbContextConfigurer.Configure(options.DbContextOptions, options.ConnectionString);
-                    }
-                });
-            }
+            Database.SetInitializer(new CreateDatabaseIfNotExists<AbpCoreEf6SampleDbContext>());
+
+            Configuration.DefaultNameOrConnectionString = "Default";
         }
 
         public override void Initialize()
         {
             IocManager.RegisterAssemblyByConvention(typeof(AbpCoreEf6SampleEntityFrameworkModule).GetAssembly());
-        }
-
-        public override void PostInitialize()
-        {
-            if (!SkipDbSeed)
-            {
-                SeedHelper.SeedHostDb(IocManager);
-            }
         }
     }
 }
