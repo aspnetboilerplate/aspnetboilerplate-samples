4d7642328e7ca30d007c8e87207c687e6cf3d0ab
İsmail ÇAĞDAŞ <ismailcagdas@gmail.com>
2023-01-17 15:10:23 +0300

    configure MassTransit in MVC project to consume messages

--- MassTransitSample/aspnet-core/src/MassTransitSample.Web.Mvc/Startup/MassTransitSampleWebMvcModule.cs
+++ MassTransitSample/aspnet-core/src/MassTransitSample.Web.Mvc/Startup/MassTransitSampleWebMvcModule.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using System;
+using Abp.Dependency;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Abp.Modules;
 using Abp.Reflection.Extensions;
+using Castle.MicroKernel.Registration;
+using MassTransit;
 using MassTransitSample.Configuration;
+using MassTransitSample.Orders.Dto;
+using MassTransitSample.Web.Orders;
 
 namespace MassTransitSample.Web.Startup
 {
@@ -26,6 +32,35 @@ namespace MassTransitSample.Web.Startup
         public override void Initialize()
         {
             IocManager.RegisterAssemblyByConvention(typeof(MassTransitSampleWebMvcModule).GetAssembly());
+        }
+
+        public override void PostInitialize()
+        {
+            IocManager.IocContainer.Register(Component.For<OrderConsumer>().LifestyleTransient());
+            
+            var busControl = Bus.Factory.CreateUsingRabbitMq(config =>
+            {
+                config.Host(new Uri("rabbitmq://localhost/"), host =>
+                {
+                    host.Username("guest");
+                    host.Password("guest");
+                });
+
+                config.ReceiveEndpoint(queueName: "repro-service", endpoint =>
+                {
+                    endpoint.Handler<OrderDto>(async context =>
+                    {
+                        using (var consumer = IocManager.ResolveAsDisposable<OrderConsumer>(typeof(OrderConsumer)))
+                        {
+                            await consumer.Object.Consume(context);
+                        }
+                    });
+                });
+            });
+
+            IocManager.IocContainer.Register(Component.For<IBus, IBusControl>().Instance(busControl));
+
+            busControl.Start();
         }
     }
 }
