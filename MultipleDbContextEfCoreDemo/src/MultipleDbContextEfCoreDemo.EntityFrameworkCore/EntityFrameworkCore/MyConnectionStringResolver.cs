db42634112a143ae1110830382cfba4ad10ff192
Saleh Ahmadi <47143979+SalehAhmadi@users.noreply.github.com>
2023-02-04 22:01:36 +0330

    Update MyConnectionStringResolver.cs

--- MultipleDbContextEfCoreDemo/src/MultipleDbContextEfCoreDemo.EntityFrameworkCore/EntityFrameworkCore/MyConnectionStringResolver.cs
+++ MultipleDbContextEfCoreDemo/src/MultipleDbContextEfCoreDemo.EntityFrameworkCore/EntityFrameworkCore/MyConnectionStringResolver.cs
@@ -1,32 +1,35 @@
-﻿using System;
+using System;
 using Abp.Configuration.Startup;
 using Abp.Domain.Uow;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using MultipleDbContextEfCoreDemo.Configuration;
-using MultipleDbContextEfCoreDemo.Web;
+using Abp.Reflection.Extensions;
+using Abp.Runtime;
 
-namespace MultipleDbContextEfCoreDemo.EntityFrameworkCore
+namespace MultipleDbContextEfCoreDemo.EntityFrameworkCore;
+
+public class MyConnectionStringResolver : DefaultConnectionStringResolver
 {
-    public class MyConnectionStringResolver : DefaultConnectionStringResolver
+    private readonly IConfigurationRoot _appConfiguration;
+    private readonly IAmbientDataContext _ambientDataContext;
+
+    public MyConnectionStringResolver(IAbpStartupConfiguration configuration, IAmbientDataContext ambientDataContext)
+        : base(configuration)
     {
-        private readonly IConfigurationRoot _appConfiguration;
+        _appConfiguration = AppConfigurations.Get(
+            typeof(MyConnectionStringResolver).GetAssembly().GetDirectoryPathOrNull(),
+            Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")
+        );
+        _ambientDataContext = ambientDataContext;
+    }
 
-        public MyConnectionStringResolver(IAbpStartupConfiguration configuration,IHostingEnvironment hostingEnvironment) 
-            : base(configuration)
+    public override string GetNameOrConnectionString(ConnectionStringResolveArgs args)
+    {
+        if (args["DbContextConcreteType"] as Type == typeof(PlaygroundSecondDbContext))
         {
-            _appConfiguration =
-                AppConfigurations.Get(hostingEnvironment.ContentRootPath, hostingEnvironment.EnvironmentName);
+            return _appConfiguration.GetConnectionString(PlaygroundConsts.SecondDbConnectionStringName);
         }
 
-        public override string GetNameOrConnectionString(ConnectionStringResolveArgs args)
-        {
-            if (args["DbContextConcreteType"] as Type == typeof(MultipleDbContextEfCoreDemoSecondDbContext))
-            {
-                return _appConfiguration.GetConnectionString(MultipleDbContextEfCoreDemoConsts.SecondDbConnectionStringName);
-            }
-
-            return base.GetNameOrConnectionString(args);
-        }
+        return base.GetNameOrConnectionString(args);
     }
 }
